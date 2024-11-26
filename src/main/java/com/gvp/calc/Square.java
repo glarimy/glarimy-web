84655f041e3324e05b00b97674c0456a5dfe5c8d
Krishna Mohan Koyya <krishna@glarimy.com>
2024-11-26 14:01:07 +0530

    asdfasdf

--- src/main/java/com/gvp/calc/Square.java
+++ src/main/java/com/gvp/calc/Square.java
@@ -7,7 +7,7 @@ public class Square {
     private static final Logger logger = LoggerFactory.getLogger(Square.class);
     public int findSquareOf(int number){
         logger.debug("Finding the square of " + number);
-        int sq = number*number;
+        int sq = number+number;
         logger.debug("Returning " + sq);
         return sq;
     }
